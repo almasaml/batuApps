48c3702a91d044fd177648d08bd74514462943d4
Almas Amalia <almasamaliaazhar@gmail.com>
2019-05-24 09:30:39 +0700

    third commit

--- app/src/main/java/com/almas/batuapps/main/views/MainActivity.kt
+++ app/src/main/java/com/almas/batuapps/main/views/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.almas.batuapps.main.views
 
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.almas.batuapps.R
+import com.almas.batuapps.databinding.ActivityMainBinding
+import com.almas.batuapps.main.viewmodels.MainViewModel
+import com.almas.batuapps.menu.listplace.viewmodels.FragmentListplaceViewModel
+import com.almas.batuapps.menu.listplace.views.FragmentListplace
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var BACK_PRESSED: Long = 0L
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel =ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.main = viewModel
+
+        setupToolbar()
+        setupButtomNavigation()
+
+        supportFragmentManager.beginTransaction().replace(R.id.fl_container, FragmentListplace.)
     }
 }
--- app/src/main/java/com/almas/batuapps/menu/listplace/views/FragmentListplace.kt
+++ app/src/main/java/com/almas/batuapps/menu/listplace/views/FragmentListplace.kt
@@ -1,2 +1,34 @@
 package com.almas.batuapps.menu.listplace.views
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.almas.batuapps.R
+import com.almas.batuapps.databinding.FragmentListplaceBinding
+import com.almas.batuapps.databinding.FragmentOtherBinding
+import com.almas.batuapps.main.viewmodels.MainViewModel
+import com.almas.batuapps.menu.listplace.viewmodels.FragmentListplaceViewModel
+
+class FragmentListplace: Fragment() {
+
+    companion object {
+        fun getInstance(): Fragment{
+            return FragmentListplace()
+        }
+    }
+
+    private lateinit var viewModel: FragmentListplaceViewModel
+    private lateinit var binding: FragmentListplaceBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_listplace, container, false)
+        viewModel =ViewModelProviders.of(this).get(FragmentListplaceViewModel::class.java)
+        binding.listplace = viewModel
+
+    }
+
+}
